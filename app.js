86c50612d566656ba5f9fa61f5462186dd921568
sinaEbrahimi <sinaebrahimi31@gmail.com>
2023-07-19 00:15:49 +0330

    git ignore configed

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.status(200).send("hello from server!!");
+  const obj = { message: "hello world!" };
+  res.status(200).json(obj);
 });
 app.get("/mame", (req, res) => {
   res.status(200).send("no mame for you");
+});
+app.post("/", (req, res) => {
+  res.json({ message: "hi" });
 });
 app.listen(port, () => {
   console.log("app is running on port 3000");
