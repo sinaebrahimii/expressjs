ba71a06dda8f2820644d3ee48702fad3ec5495c7
sinaEbrahimi <sinaebrahimi31@gmail.com>
2023-07-20 23:58:23 +0330

    get request for single tour created

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ app.get("/api/v1/tours", (req, res) => {
     },
   });
 });
+app.get("/api/v1/tours/:id", (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((tour) => tour.id === Number(id));
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
 
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
