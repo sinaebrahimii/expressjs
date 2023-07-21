26ea02932a60971acfceee6514a0cde6db73d231
sinaEbrahimi <sinaebrahimi31@gmail.com>
2023-07-21 19:50:52 +0330

    users route defined

--- app.js
+++ app.js
@@ -86,8 +86,37 @@ const deleteTour = (req, res) => {
     message: "Deleted Tour successfuly !",
   });
 };
+
+const getAllUsers = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "this route is not yet defined" });
+};
+const createUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "this route is not yet defined" });
+};
+const getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "this route is not yet defined" });
+};
+const updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "this route is not yet defined" });
+};
+const deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "this route is not yet defined" });
+};
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 app.route("api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+app.route("api/v1/users").get(getAllUsers).post(createUser);
+app.route("api/v1/users/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
 app.listen(port, () => {
   console.log("app is running on port 3000");
 });
