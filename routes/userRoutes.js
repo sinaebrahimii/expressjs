d1df5f44507514f8664d049b2161a9b4b1f5408e
sinaEbrahimi <sinaebrahimi31@gmail.com>
2023-08-14 15:53:41 +0330

    imports changed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const userController = require("../controllers/userController");
-const { getAllUsers, createUser, updateUser, getUser, deleteUser } =
-  userController;
-const { signUp, logIn } = require("../controllers/authController");
+const {
+  getAllUsers,
+  createUser,
+  updateUser,
+  getUser,
+  deleteUser,
+} = require("../controllers/userController");
+const authController = require("../controllers/authController");
 const router = express.Router();
-router.route("/signup").post(signUp);
-router.route("/login").post(logIn);
+router.route("/signup").post(authController.signUp);
+router.route("/login").post(authController.logIn);
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 module.exports = router;
